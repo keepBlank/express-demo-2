fd20ba7d8f78e468aede6f08c74677efc630ed3a
keepBlank <front-endligen@foxmail.com>
2023-07-07 17:39:32 +0800

    使用express-starter初始化项目 && 测试app.use

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {request} = require('express');
+
+const app = express();
+
+app.use((request, response, next) => {
+    console.log(request.url);
+    response.write('Hi,I am Server Client')
+    next()
+});
+
+app.use((request, response, next) => {
+    console.log(2);
+    response.write('Hi,I am Server Client 2')
+    next()
+});
+
+app.use((request, response, next) => {
+    response.end()
+});
+
+app.listen(3000,()=>{
+    console.log('正在监听3000端口');
+});
